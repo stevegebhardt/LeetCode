77fd0579f521605305a21f700f3cb6b857d60cfd
stevegebhardt <gebhardt.steven3@gmail.com>
2023-09-22 14:16:55 -0400

    number palindrome completed, but with string conversion only

---
+++ Number_Palindrome.js
@@ -0,0 +1,21 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
+const number = 1000021;
+
+const isPalindrome = (number) => {
+  let pali = number.toString();
+  for (let i = 0; i < pali.length; i++) {
+    if (pali[i] !== pali[pali.length - 1 - i]) {
+      console.log(false);
+    }
+    console.log(true);
+  }
+};
+
+isPalindrome(number);
